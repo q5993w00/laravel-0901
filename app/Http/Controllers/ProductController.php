16b302534ad2a8e7886d98b4eec098274f2c8e7a
q5993w00 <a0966843311@gmail.com>
2023-09-13 20:37:49 +0800

    新增

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    //顯示資料的地方 :
+    public function index()
+    {
+        $products = Product::get();
+        //產品列表頁(1)
+        return view('product.cartlist', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //新增資料頁(2->產品新增頁)
+        return view('product.addcartlist');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //驗證進來的資料是否合規龜龜龜
+
+        //新產品資料儲存
+
+        //方法一
+        //查看請求的資料(form表單裡input)
+        $path = Storage::putFile('public/upload', $request->file('image'));
+
+        //方法二
+        // dd($request->all());
+        Product::create([
+            'name' => $request->name,
+            'img_path' => str_replace('public', 'storage', $path),
+            'price' => $request->price,
+            'status' => $request->status,
+            'desc' => $request->desc,
+        ]);
+
+        return redirect(route('product.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //新產品資料儲存功能
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        //產品編輯頁(3)
+        return view('product.editcartlist',compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+
+        //驗證
+
+        //產品更新功能
+        // dd($request->all());
+        $product = Product::find($id);
+        $product->update([
+            'name' => $request->name,
+            'price' => $request->price,
+            'status' => $request->status,
+            'desc' => $request->desc,
+        ]);
+        return redirect(route('product.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //刪除資料功能
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/', function () {
+    return view('product.welcome');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/product/list', [ProductController::class, 'index'])->name('product.index');
+
+Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+Route::post('/product/store', [ProductController::class, 'store'])->name('product.store');
+
+Route::get('/product/edit/{id}', [ProductController::class, 'edit'])->name('product.edit');
+Route::post('/product/update/{id}', [ProductController::class, 'update'])->name('product.update');
+
+
+
+
+
